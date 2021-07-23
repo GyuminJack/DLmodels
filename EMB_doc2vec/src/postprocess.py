da5951c75b3664e59bedcd9d47e86aabcbbcef16
gyuminjack <sld9849@gmail.com>
2021-07-23 15:12:58 +0900

    upload

--- EMB_doc2vec/src/postprocess.py
+++ EMB_doc2vec/src/postprocess.py
@@ -3,6 +3,10 @@ from gensim.models.doc2vec import Doc2Vec
 import numpy as np
 import time
 from utils import *
+from numba import njit
+from multiprocessing import Pool
+import gc
+import sys
 
 def calculate_cosine(hscd_vectors, coid_vectors, n_split):
     def __split_indices(a, n):
@@ -11,14 +15,13 @@ def calculate_cosine(hscd_vectors, coid_vectors, n_split):
         return (a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
 
     if n_split > len(coid_vectors):
-        n_split = 1
+        n_split = 10
 
     split_indices = __split_indices(range(len(coid_vectors)), n_split)
 
-    total_cos_mat = []
+    total_cos_mat = [None] * n_split
     for _id, _splitted_coid_index in enumerate(split_indices):
-        cos_mat = cosine_similarity(hscd_vectors, coid_vectors[_splitted_coid_index])
-        total_cos_mat.append(cos_mat)
+        total_cos_mat[_id] = cosine_similarity(hscd_vectors, coid_vectors[_splitted_coid_index]).astype(np.float16)
     
     return total_cos_mat
 
@@ -43,7 +46,9 @@ class postprocess:
     def _split_vectors(self, hscode_path, coid_path):
         raise NotImplementedError("_split_vectors method")
 
+    @time_printer(True)
     def make_cosmat(self, hscd_vectors, coid_vectors, n_split=100):
+        # return size : [[hscd * coid/n_split] * n_split]
         return calculate_cosine(hscd_vectors, coid_vectors, n_split)
 
     def save_files(self):
@@ -51,33 +56,45 @@ class postprocess:
         writer(self.coid_tags, os.path.join(self.vector_save_path, 'coid.tags'))
         np.save(os.path.join(self.vector_save_path, 'hscd.npy'), self.hscd_vectors)
         np.save(os.path.join(self.vector_save_path, 'coid.npy'), self.coid_vectors)
+    
+    def argsort_by_hscd_vector(self, hscd_id):
+        # return size : 1 * coid length
+        hscode_cos_vector = np.concatenate([each_cos[hscd_id] for each_cos in self.cos_mats], axis=-1)
+        return -hscode_cos_vector.argsort(axis=-1)
 
-    def save_bulk_cosmat(self, cos_mat:list, hscd_tags, coid_tags):
+    def argsort_by_coid_matrix(self, sub_cosine_matrix):
+        # return size : sub coid vector * hscd length
+        return -sub_cosine_matrix.T.argsort(axis=-1)
+
+    @time_printer(True)
+    def sort_and_save_cosmat(self, cos_mat:list, hscd_tags, coid_tags):
         def save(tag, rank_vector):
             # 수정 필요 ***
             # 일단 파일로 떨구고 그 다음에 다른 모듈에서 불러다가 서비스를 위해 상위 몇만개 추출하는 시나리오로 예상
-            print(tag, rank_vector)
+            pass
 
         # HSCODE 기준 유사도 순위 생성 (max_rank = len(coid))
+        # BEST : 2s
         for _id, _tag in enumerate(hscd_tags):
-            hscode_cos_mat = np.concatenate([_tmp[_id] for _tmp in cos_mat], axis=-1)
-            hscode_coid_sorted_index = np.argsort(-hscode_cos_mat, axis=-1) # HSCODE(1) - COID (max_rank = 6000000)
+            hscode_coid_sorted_index = self.argsort_by_hscd_vector(_id)
             save(_tag, hscode_coid_sorted_index)
         
         # COID 기준 유사도 순위 생성 (max_rank = len(hscode))
         start = 0
-        for _id, sub_cos_mat in enumerate(cos_mat):
+        for sub_cos_mat in cos_mat:
             end = start + sub_cos_mat.shape[-1]
-            _tag = coid_tags[start : end]
-            print(sub_cos_mat.T)
-            coid_hscode_sorted_index = np.argsort(-sub_cos_mat.T, axis=-1) # sub_coid_range * 6000
-            for _t, _r in zip(_tag, coid_hscode_sorted_index):
-                save(_t, _r)
+            _tag = coid_tags[start : end] # Just find tag name for save
             start = end
 
+            coid_hscode_sorted_index = self.argsort_by_coid_matrix(sub_cos_mat)
+            for _t, _r in zip(_tag, coid_hscode_sorted_index):
+                save(_t, _r)
+    
+    @time_printer(True)
     def run(self):
         self.cos_mats = self.make_cosmat(self.hscd_vectors, self.coid_vectors)
-        self.save_bulk_cosmat(self.cos_mats, self.hscd_tags, self.coid_tags)
+        self.model, self.hscd_vectors, self.coid_vectors = None, None, None
+        self.sort_and_save_cosmat(self.cos_mats, self.hscd_tags, self.coid_tags)
 
 class post_doc2vec(postprocess):
     def __init__(self, model, config):
@@ -87,6 +104,7 @@ class post_doc2vec(postprocess):
         self.save_files()
         self.run()
 
+    @time_printer(True)
     def __check_d2v_index_and_values_order(self):
         doctags = self.model.docvecs.index2entity
         vectors = self.model.docvecs.vectors_docs
@@ -94,6 +112,7 @@ class post_doc2vec(postprocess):
         for _index, _id in enumerate(doctags):
             assert np.array_equal(self.model.docvecs[_id], vectors[_index]), "Check Failed..!"
     
+    @time_printer(True)
     def _split_vectors(self, hscode_path, coid_path):
         # 바뀔수도..?
         def __make_tag_with_string(string):
@@ -104,7 +123,7 @@ class post_doc2vec(postprocess):
             _doctags = model.docvecs.index2entity
             tags = [__make_tag_with_string(i) for i in gen_string]
             _doctag_index = [_doctags.index(tag) for tag in tags]
-            _vectors = model.docvecs.vectors_docs[_doctag_index].reshape(-1, model.docvecs.vector_size)
+            _vectors = model.docvecs.vectors_docs[_doctag_index].reshape(-1, model.docvecs.vector_size).astype(np.float16)
             return tags, _vectors
 
         gen_hscode_string, gen_coid_string = read_key_value_words(hscode_path, coid_path)
@@ -130,16 +149,17 @@ class post_word2vec(postprocess):
                 pass
 
         if len(np_words) == 0:
-            return np.zeros(self.model.vector_size)
+            return np.zeros(self.model.vector_size).astype(np.float16)
         else:
-            return np.mean(np.stack(np_words), axis=0)
+            return np.mean(np.stack(np_words), axis=0).astype(np.float16)
 
     def _split_vectors(self, hscode_path, coid_path):
         def __make_key_and_words(string):
-            key, words = __make_key_and_words
+            key, words = string.split("|")
             words = words.split(",")
             return key, words
 
+        @time_printer(True)
         def _get_key_words_centroids(gen_string):
             tags = []
             vectors = []
@@ -176,3 +196,30 @@ if __name__ == "__main__":
         model = Doc2Vec.load(config["model"]["model_path"])
         post_processor = post_doc2vec(model, config)
 
+
+        # argsort Ideas
+        
+        # Too Slow
+        # hscode_cos_mat = np.concatenate(cos_mat, axis=-1)
+        # hscode_coid_sorted_index = np.argpartition(hscode_cos_mat, 
+        #         range(hscode_cos_mat.shape[1]), axis=-1)[:,:hscode_cos_mat.shape[1]]
+        # exit()
+
+        # numba : 4s
+        # for _id, _tag in enumerate(hscd_tags):
+        #     hscode_cos_vector = np.concatenate([each_cos[_id] for each_cos in cos_mat], axis=-1)
+        #     hscode_coid_sorted_index = postprocess.nb_sort(-hscode_cos_vector) # HSCODE(1) - COID (max_rank = 6000000)
+        #     # hscode_coid_sorted_index = np.argpartition(hscode_cos_vector, 
+        #     #     range(len(hscode_cos_vector)), axis=-1)[:len(hscode_cos_vector)]
+        #     save(_tag, hscode_coid_sorted_index)
+
+        # save time : 1min 10s
+        # with open("cosine_test_npy_hscd6k.txt", "ab") as f:
+        #     for _id, _tag in enumerate(hscd_tags):
+        #         hscode_cos_vector = np.concatenate([each_cos[_id] for each_cos in cos_mat], axis=-1)
+        #         np.savetxt(f, hscode_cos_vector)
+        #         f.write(b"\n")
+        #         # hscode_coid_sorted_index = np.argsort(-hscode_cos_vector, axis=-1) # HSCODE(1) - COID (max_rank = 6000000)
+        #         # hscode_coid_sorted_index = np.argpartition(hscode_cos_vector, 
+        #         #     range(len(hscode_cos_vector)), axis=-1)[:len(hscode_cos_vector)]
+        #         # save(_tag, hscode_coid_sorted_index)
